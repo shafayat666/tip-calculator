dc201c3de5fac91a4b849286353f198c5b68eae7
shafayat666 <shafayatmahin5@gmail.com>
2024-10-05 15:39:11 +0600

    converted the bill variable from int to float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 
-bill = int(input("What was the total bill? $"))
+bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? ")) + 100
 num_of_people = int(input("How many people to split the bill? "))
 
